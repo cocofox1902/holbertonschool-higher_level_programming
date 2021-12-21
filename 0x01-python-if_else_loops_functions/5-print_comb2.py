32beeaccf56619e6e30e48ac20b4e0f906eeca07
cocofox1902 <colas.renard1902@gmail.com>
2021-12-21 11:28:52 +0000

    task check

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
-        print("0{}".format(number), end="")
+    if number != 99:
+        print("{:02}".format(number), end=", ")
     else:
-        print("{}".format(number), end="")
-    if number < 99:
-        print(", ", end="")
-    else:
-        print("")
+        print("{}".format(number))
