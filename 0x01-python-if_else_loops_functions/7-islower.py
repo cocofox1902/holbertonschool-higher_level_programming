e8df6a30d32f0e08bbeb48d7ffe749e363e35159
cocofox1902 <colas.renard1902@gmail.com>
2021-12-21 11:33:54 +0000

    task check

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def islower(c):
-    if ord(c) < 96:
-        return(False)
-    else:
+    if ord(c) >= 96 and ord(c) <= 122:
         return(True)
+    else:
+        return(False)
