2bcb61f4e69053c3d3ee3604efd2b6848045b32b
cocofox1902 <colas.renard1902@gmail.com>
2022-01-05 22:06:05 +0000

    task push

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -10,28 +10,28 @@
 
 void print_python_bytes(PyObject *p)
 {
+
 	long int size;
 	int i;
+	char *trying_str = NULL;
 
 	printf("[.] bytes object info\n");
-
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	size = ((PyVarObject *)p)->ob_size;
+	PyBytes_AsStringAndSize(p, &trying_str, &size);
 
-	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
-
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", trying_str);
 	if (size < 10)
 		printf("  first %li bytes:", size + 1);
 	else
-		printf(" first 10 bytes:");
+		printf("  first 10 bytes:");
 	for (i = 0; i <= size && i < 10; i++)
-		printf("%02hhx", ((PyBytesObject *)p)->ob_sval[i]);
+		printf(" %02hhx", trying_str[i]);
 	printf("\n");
 }
 
@@ -44,19 +44,19 @@ void print_python_bytes(PyObject *p)
 
 void print_python(PyObject *p)
 {
-	const char *t;
+	long int size = PyList_Size(p);
 	int i;
-	long int lenght = PyList_Size(p);
+	PyListObject *list = (PyListObject *)p;
+	const char *type;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %li\n", lenght);
-	printf("[*] Allocated = %li\n", ((PyListObject *)p)->allocated);
-
-	for (i = 0; i < lenght; i++)
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", list->allocated);
+	for (i = 0; i < size; i++)
 	{
-		t = (((PyListObject *)p)->ob_item[i])->ob_type->tp_name;
-		printf("Element %i: %s\n", i, t);
-		if (!strcmp((((PyListObject *)p)->ob_item[i])->ob_type->tp_name, "bytes"))
-			print_python_bytes(((PyListObject *)p)->ob_item[i]);
+		type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(list->ob_item[i]);
 	}
 }
