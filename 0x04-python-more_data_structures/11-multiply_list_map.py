ffde6a66c26696f8628906428ce183c191b4724e
cocofox1902 <colas.renard1902@gmail.com>
2022-01-05 12:30:04 +0000

    task push

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    my_list = [i * number for i in my_list]
-    return (my_list)
+    return list(map(lambda x: x*number , my_list))
