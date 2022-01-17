bd9e2fee9348f838223b7a25e4f7aea484e434e9
cocofox1902 <colas.renard1902@gmail.com>
2022-01-17 11:07:52 +0000

    chekeur test

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    test = 1
-    if type(value) == type(test):
-        try:
-            print("{:d}".format(value))
-            return(True)
-        except TypeError:
-            return(False)
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except (TypeError, ValueError):
+        return(False)
