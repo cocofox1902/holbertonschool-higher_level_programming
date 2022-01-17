9cfd81724a38219540f6ffd58fa7430519839ccf
cocofox1902 <colas.renard1902@gmail.com>
2022-01-17 11:13:03 +0000

    chekeur test

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-from distutils.errors import LibError
-from re import I
-from tokenize import maybe
-
-
 def list_division(my_list_1, my_list_2, list_length):
     mylist = []
     for i in range(list_length):
