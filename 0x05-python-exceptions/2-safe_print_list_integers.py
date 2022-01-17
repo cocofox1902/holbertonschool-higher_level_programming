7f7eb29413fe3abb2abb6bc3e987de236c745fa6
cocofox1902 <colas.renard1902@gmail.com>
2022-01-17 11:11:48 +0000

    chekeur test

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    test = 1
+    a = 1
     for y in range(0, x):
         try:
             print("{:d}".format(my_list[y]), end='')
-            y += 1
+            a += 1
         except (ValueError, TypeError):
             continue
     print()
-    return(y)
+    return(a)
