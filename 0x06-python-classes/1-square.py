4eb963d32c32a42151a5610c3371688e75d5a1ee
cocofox1902 <colas.renard1902@gmail.com>
2022-01-18 16:00:14 +0000

    comment

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class Square """
+
+
+class Square:
+    """ def __init__ """
+    def __init__(self, size):
+        """ Init function
+
+            Args:
+                size (int): Size of the square
+        """
+        self.__size = size
