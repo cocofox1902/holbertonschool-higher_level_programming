acd9a0c4c6b72e17719b543b63b342a11b7695f2
cocofox1902 <colas.renard1902@gmail.com>
2022-01-24 16:12:04 +0000

    rectangle print

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+"""module class of a rectangle"""
+
+
+class Rectangle:
+    """class rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """ __init__ function
+
+            Args:
+                width(int/flaot): width of the rectangle
+                height(int/flaot): height of the rectangle
+
+            Return:
+                No return
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width function
+
+            Args:
+                width(int/flaot): width of the rectangle
+
+            Return:
+                width
+        """
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ width function test value
+
+            Args:
+                width(int/flaot): width of the rectangle
+
+            Return:
+                no return
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height function
+
+            Args:
+                height(int/flaot): height of the rectangle
+
+            Return:
+                height
+        """
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ height function test value
+
+            Args:
+                height(int/flaot): height of the rectangle
+
+            Return:
+                no return
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ area function
+
+            Args:
+                height(int/flaot): height of the rectangle
+                width(int/flaot): width of the rectangle
+
+            Return:
+                area of the rectangle
+        """
+        return(self.__width * self.__height)
+
+    def perimeter(self):
+        """ perimeter function
+
+            Args:
+                height(int/flaot): height of the rectangle
+                width(int/flaot): width of the rectangle
+
+            Return:
+                perimeter of the rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return(0)
+        return((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """ __str__ function
+
+            Args:
+                height(int/flaot): height of the rectangle
+                width(int/flaot): width of the rectangle
+                square(list): list of #
+
+            Return:
+                a rectangle print
+        """
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        square = []
+        for i in range(self.__height):
+            for j in range(self.__width):
+                square.append("#")
+            if self.__height - 1 != i:
+                square.append("\n")
+        return("".join(square))
