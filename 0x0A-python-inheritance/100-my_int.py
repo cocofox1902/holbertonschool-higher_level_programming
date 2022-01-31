5bc16e6620ed936a3fe472eff18a01a5a129397d
cocofox1902 <colas.renard1902@gmail.com>
2022-01-31 18:34:52 +0000

    check test

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
+"""module"""
 class MyInt(int):
+    """ class MyInt
+
+        def __eq__
+        def __ne__
+    """
     def __eq__(self, dif):
+        """function __eq__
+
+            Args:
+                no Args
+
+            Return:
+                Diference
+        """
         return int(self) != dif
 
     def __ne__(self, dif):
+        """function __eq__
+
+            Args:
+                no Args
+
+            Return:
+                Equal
+        """
         return int(self) == dif
