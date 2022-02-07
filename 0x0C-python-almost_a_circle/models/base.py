17a7c9aef609219d1fb95fbe0c4998f1c655304a
cocofox1902 <colas.renard1902@gmail.com>
2022-02-07 11:39:21 +0000

    add task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python3
+"""module to have the base"""
+import json
+import csv
+from os.path import exists
+
+
+class Base:
+    """class Base
+
+        def __init__
+        def to_json_string(staticmethod)
+        def save_to_file(classmethod)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """function __init__
+
+            Args:
+                id(int): the id of the base
+
+            Return:
+                no return
+        """
+        self.__id = id
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """function to_json_string
+
+            Args:
+                list_dictionaries(list): list of the dict
+
+            Return:
+                dict of the base
+        """
+        if list_dictionaries == [] or list_dictionaries is None:
+            return("[]")
+        else:
+            return(json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """function save_to_file
+
+            Args:
+                list_objs(list): list of the object
+
+            Return:
+                object of the base
+        """
+        filename = cls.__name__ + ".json"
+        count = len(list_objs) - 1
+        with open(filename, 'w') as f:
+            if list_objs is None:
+                f.write('[]')
+            else:
+                f.write('[')
+                for i in list_objs:
+                    dict = i.to_dictionary()
+                    f.write(Base.to_json_string(dict))
+                    if count > 0:
+                        f.write(", ")
+                        count -= 1
+                f.write(']')
+
+    @staticmethod
+    def from_json_string(json_string):
+        """function from_json_string
+
+            Args:
+                json_string(str): json string
+
+            Return:
+                json loads
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """function create
+
+            Args:
+                dictionary(str): dictionary
+
+            Return:
+                dictionary add
+        """
+        name = cls.__name__
+        if (dictionary != {} and dictionary):
+            if name == "Rectangle":
+                add = cls(1, 1)
+            else:
+                add = cls(1)
+            add.update(**dictionary)
+            return(add)
+
+    @classmethod
+    def load_from_file(cls):
+        """function load_from_file
+
+            Args:
+                no args
+
+            Return:
+                load file
+        """
+        name = cls.__name__ + ".json"
+        try:
+            with open(name, "r") as f:
+                dict = Base.from_json_string(f.read())
+                return [cls.create(**i) for i in dict]
+        except IOError:
+            return([])
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """function save_to_file_csv
+
+            Args:
+                list_objs(list): object list
+
+            Return:
+                no return
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w") as f:
+            if list_objs == [] or list_objs is None:
+                f.write("")
+            else:
+                if cls.__name__ == "Rectangle":
+                    paramters = ["id", "width", "height", "x", "y"]
+                else:
+                    paramters = ["id", "size", "x", "y"]
+                w = csv.DictWriter(f, fieldnames=paramters)
+                for obj in list_objs:
+                    w.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """function load_from_file_csv
+
+            Args:
+                no args
+
+            Return:
+                dict and file load
+        """
+        filename = cls.__name__ + ".csv"
+        listOfObjs = []
+        try:
+            with open(filename, "r") as f:
+                reader = csv.reader(f)
+                for elem in reader:
+                    if cls.__name__ == "Rectangle":
+                        newObj = {
+                            "id": int(elem[0]),
+                            "width": int(elem[1]),
+                            "height": int(elem[2]),
+                            "x": int(elem[3]),
+                            "y": int(elem[4]),
+                        }
+                    else:
+                        newObj = {
+                            "id": int(elem[0]),
+                            "size": int(elem[1]),
+                            "x": int(elem[2]),
+                            "y": int(elem[3]),
+                        }
+                    newObj = cls.create(**newObj)
+                    listOfObjs.append(newObj)
+            return listOfObjs
+        except FileExistsError:
+            return []
+
+    def draw(list_rectangles, list_squares):
+        """function draw
+
+            Args:
+                list_rectangles
+                list_squares
+
+            Return:
+                xxx
+        """
+        return(0)
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,253 @@
+#!/usr/bin/python3
+"""module to print a Rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle
+
+        def __init__
+        def width @property/@width.setter
+        def height @property/@height.setter
+        def x @property/@x.setter
+        def y @property/@y.setter
+        def area
+        def display
+        def __str__
+        def update
+        def to_dictionary
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """function __init__
+
+            Args:
+                width(int): width square
+                height(int): height square
+                x(int): offset of the square right/left
+                y(int): offset of the square top/bottom
+                id(int): the id of the square
+
+            Return:
+                no return
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """function width(property)
+
+            Args:
+                number(int): width value
+
+            Return:
+                width value
+        """
+        return(self.__width)
+
+    @width.setter
+    def width(self, number):
+        """function width(width.setter)
+
+            Args:
+                number(int): width value
+
+            Return:
+                no return
+        """
+        if isinstance(number, int) is False:
+            raise TypeError("width must be an integer")
+        if number <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = number
+
+    @property
+    def height(self):
+        """function height(property)
+
+            Args:
+                number(int): height value
+
+            Return:
+                height value
+        """
+        return(self.__height)
+
+    @height.setter
+    def height(self, number):
+        """function height(height.setter)
+
+            Args:
+                number(int): height value
+
+            Return:
+                no return
+        """
+        if isinstance(number, int) is False:
+            raise TypeError("height must be an integer")
+        if number <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = number
+
+    @property
+    def x(self):
+        """function x(property)
+
+            Args:
+                number(int): x value
+
+            Return:
+                x value
+        """
+        return(self.__x)
+
+    @x.setter
+    def x(self, number):
+        """function x(x.setter)
+
+            Args:
+                number(int): x value
+
+            Return:
+                no return
+        """
+        if isinstance(number, int) is False:
+            raise TypeError("x must be an integer")
+        if number < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = number
+
+    @property
+    def y(self):
+        """function y(property)
+
+            Args:
+                number(int): y value
+
+            Return:
+                y value
+        """
+        return(self.__y)
+
+    @y.setter
+    def y(self, number):
+        """function y(y.setter)
+
+            Args:
+                number(int): y value
+
+            Return:
+                no return
+        """
+        if isinstance(number, int) is False:
+            raise TypeError("y must be an integer")
+        if number < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = number
+
+    def area(self):
+        """function area
+
+            Args:
+                no args
+
+            Return:
+                the area of the square
+        """
+        return(self.width * self.height)
+
+    def display(self):
+        """function display
+
+            Args:
+                no args
+
+            Return:
+                no return
+
+            Print:
+                the rectangle
+        """
+        for y in range(self.y):
+            print()
+        for i in range(self.height):
+            for x in range(self.x):
+                print(" ", end="")
+            for j in range(self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """function __str__
+
+            Args:
+                no args
+
+            Return:
+                return rectangle all value
+        """
+        return("[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width,
+            self.height))
+
+    def update(self, *args, **kwargs):
+        """function update
+
+            Args:
+                args(list): list of the value of the rectangle
+                kwargs(list of list): list of the value with the name
+
+            Return:
+                no return
+        """
+        if args != 0 and len(args) != 0:
+            i = 0
+            for arguments in args:
+                if i == 0:
+                    if arguments is not None:
+                        self.id = arguments
+                    else:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                elif i == 1:
+                    self.width = arguments
+                elif i == 2:
+                    self.height = arguments
+                elif i == 3:
+                    self.x = arguments
+                elif i == 4:
+                    self.y = arguments
+                i += 1
+
+        elif kwargs != 0 and len(kwargs) != 0:
+            for key, number in kwargs.items():
+                if key == "width":
+                    self.width = number
+                elif key == "height":
+                    self.height = number
+                elif key == "x":
+                    self.x = number
+                elif key == "y":
+                    self.y = number
+                elif key == "id":
+                    if number is not None:
+                        self.id = number
+                    else:
+                        self.__init__(self.width, self.height, self.x, self.y)
+
+    def to_dictionary(self):
+        """function to_dictionary
+
+            Args:
+                no args
+
+            Return:
+                dict of the rectangle
+        """
+        return {"id": self.id, "width": self.width,
+                "height": self.height, "x": self.x, "y": self.y}
