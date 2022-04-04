812b54db409beb7e346ed07221512ebbf33be9cb
cocofox1902 <colas.renard1902@gmail.com>
2022-04-04 12:14:45 +0000

    add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2).sort((a, b) => a - b);
+  console.log(array[array.length - 2]);
+}
