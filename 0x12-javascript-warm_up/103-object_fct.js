33d50c2f6cf45f6fef13fc5d4054a82203b14a12
cocofox1902 <colas.renard1902@gmail.com>
2022-04-04 14:52:35 +0000

    add task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,12 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
-  this.value += 1;
+  myObject.value += 1;
 };
+
+myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
