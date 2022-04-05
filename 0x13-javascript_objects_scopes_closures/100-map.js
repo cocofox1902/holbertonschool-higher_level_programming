41d91ba23b6c05973000290bd60e2586c926a047
cocofox1902 <colas.renard1902@gmail.com>
2022-04-05 12:10:08 +0000

    add task to cheack

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,6 +3,6 @@
 const list = require('./100-data').list;
 console.log(list);
 const newlist = list.map(function (number, index) {
-	  return number * index;
+  return number * index;
 });
 console.log(newlist);
