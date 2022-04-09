d0384b001ac00f966b1ef5a2de391528c46a7358
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 13:12:50 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Write a script that changes the name of a State object from
+the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
