0ade72f367e3677cfa666bc58d970012a57cc31a
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 12:08:59 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Write a script that lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa"""
-import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
