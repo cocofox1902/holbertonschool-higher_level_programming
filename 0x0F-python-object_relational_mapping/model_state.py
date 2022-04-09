d5b121b7544184a6baf0d4c47802f51a86a28388
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 13:10:14 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Write a python file that contains the class
+definition of a State and an instance
+Base = declarative_base():
+"""
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
