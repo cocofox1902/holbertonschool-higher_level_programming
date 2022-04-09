ccc1f3bbf7fbe22f2ca125ad8460d4523c52b2ea
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 12:29:56 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], conn=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE Name LIKE 'N%'")
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
