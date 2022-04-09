64ffe55a941a2f305c366078bc67b5d358d31b8e
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 13:05:32 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE Name LIKE '{:s}'"
-                " ORDER BY id ASC'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE"
+                " '{:s}' ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1] == argv[4]:
