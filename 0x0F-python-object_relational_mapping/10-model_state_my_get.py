45f69e531817ff3b19e6507e9f0bbea9e46b4ca8
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 13:12:12 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""Write a script that prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa"""
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
