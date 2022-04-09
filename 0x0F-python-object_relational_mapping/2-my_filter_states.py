2ed7e350735d00b3befc1105bb8db229d35cb9d5
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 13:02:10 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Write a script that takes in an argument and displays
+all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
 import MySQLdb
 from sys import argv
 
@@ -6,7 +10,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE Name LIKE '{:s}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE Name LIKE '{:s} \
+                 ORDER BY id ASC'".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
