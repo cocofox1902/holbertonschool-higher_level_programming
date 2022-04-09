c444ccc74437e39b193455355dd013c02d5be04a
cocofox1902 <colas.renard1902@gmail.com>
2022-04-09 13:12:32 +0000

    add checkeur

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Write a script that adds the State object “Louisiana” to the
+database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
