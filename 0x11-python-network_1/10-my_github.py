d18110e40cb199a4efa87e60ea23f99b0b12aa80
cocofox1902 <colas.renard1902@gmail.com>
2022-04-22 08:48:47 +0000

    add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 10 - my_github.py """
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    resp = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    if resp.status_code >= 400:
+        print('None')
+    else:
+        print(resp.json().get('id'))
