4064bb211711e9fe7e704f4ef44ecd7ab4d4ffde
cocofox1902 <colas.renard1902@gmail.com>
2022-05-17 12:53:57 +0000

    add task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const axios = require("axios");
+const axios = require('axios');
 
 axios
-  .get("https://swapi-api.hbtn.io/api/films/" + process.argv[2])
+  .get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
   .then(async (res) => {
     for (const characters of res.data.characters) {
       await axios.get(characters).then((res) => {
