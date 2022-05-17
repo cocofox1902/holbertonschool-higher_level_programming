7d4a82554434288f1713a768846476d917b2823f
cocofox1902 <colas.renard1902@gmail.com>
2022-05-17 12:49:02 +0000

    add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const axios = require("axios");
+
+const List = {};
+let value;
+axios
+  .get(process.argv[2])
+  .then((res) => {
+    for ([, value] of Object.entries(res.data)) {
+      if (value.completed === true) {
+        if (List[value.userId] === undefined) {
+          List[value.userId] = 0;
+        }
+        List[value.userId] += 1;
+      }
+    }
+    console.log(List);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
