8bfd84eabf54421db1a3e915d83fb6a25d761a00
cocofox1902 <colas.renard1902@gmail.com>
2022-05-17 11:53:15 +0000

    add task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,7 @@ const axios = require('axios').default;
 axios
   .get(process.argv[2])
   .then(function (response) {
-    console.error('code:', response.status);
+    console.log('code:', response.status);
   })
   .catch(function (error) {
     console.error('code: ', error.response.status);
